bbe0253a833d21435dcef0919a6cd2539b1cbd9c
Brian Punzalan <brianpunzalan@Brians-MacBook-Pro.local>
2024-06-17 19:57:27 +0800

    feat: initial setup

---
+++ eslint.config.js
@@ -0,0 +1,76 @@
+import { includeIgnoreFile } from '@eslint/compat'
+import js from '@eslint/js'
+import jsdoc from 'eslint-plugin-jsdoc'
+import {
+  configs as reactHooksEslintConfigs,
+  rules as reactHooksEslintRules,
+} from 'eslint-plugin-react-hooks'
+import { rules as reactRefreshEslintRules } from 'eslint-plugin-react-refresh'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import tseslint from 'typescript-eslint'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
+
+const reactRefreshEslint = {
+  plugins: {
+    'react-refresh': {
+      configs: {},
+      rules: {
+        ...reactRefreshEslintRules,
+      },
+    },
+  },
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+}
+
+const reactHooksEslint = {
+  plugins: {
+    'react-hooks': {
+      configs: {},
+      rules: {
+        ...reactHooksEslintRules,
+      },
+    },
+  },
+  rules: {
+    ...reactHooksEslintConfigs.recommended.rules,
+  },
+}
+
+const jsdocEslint = [
+  jsdoc.configs['flat/recommended-typescript-error'],
+  {
+    files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+    plugins: {
+      jsdoc,
+    },
+    rules: {
+      'jsdoc/require-description': [
+        'error',
+        {
+          require: {
+            FunctionDeclaration: true,
+            MethodDefinition: true,
+            ClassDeclaration: true,
+            ArrowFunctionExpression: false,
+            FunctionExpression: false,
+          },
+        },
+      ],
+    },
+  },
+]
+
+export default tseslint.config(
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  reactHooksEslint,
+  reactRefreshEslint,
+  ...jsdocEslint
+)
---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+const config = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  printWidth: 90,
+  useTabs: false,
+  endOfLine: 'lf',
+  tabWidth: 2,
+  plugins: ['prettier-plugin-organize-imports'],
+  organizeImportsSkipDestructiveCodeActions: true,
+}
+
+export default config
