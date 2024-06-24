044959978921425beecb92d8ca6ea2e78533e481
Brian Punzalan <brianpunzalan@Brians-MacBook-Pro.local>
2024-06-25 00:20:58 +0800

    feat: initial vite lib mode attempt

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'ElementsUI',
+    },
+  },
 })
